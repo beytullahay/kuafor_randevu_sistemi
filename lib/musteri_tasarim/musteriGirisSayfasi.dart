8569cd619f6de98c1c1d1bfa8366b97ed6f2aa19
BeytullahAy <beytullahay47@gmail.com>
2022-04-29 10:56:38 +0300

    update

--- lib/musteri_tasarim/musteriGirisSayfasi.dart
+++ lib/musteri_tasarim/musteriGirisSayfasi.dart
@@ -16,87 +16,94 @@ class _musteriGirisSayfasiState extends State<musteriGirisSayfasi> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("MÜŞTERİ GİRİŞİ"),
-        ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 30.0),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              logom,
-              SizedBox(height: 20),
-              TextField(
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 25),
-                decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
-                  hintText: ' 05** *** ** **',
-                  prefixIcon: Icon(Icons.phone),
-                  border: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(width: 1, color: Colors.blueAccent),
-                      borderRadius:
-                          const BorderRadius.all(const Radius.circular(10.0))),
-                ),
-                keyboardType: TextInputType.phone,
-              ),
-              SizedBox(height: 10),
-              TextField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 25),
-                decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
-                  hintText: 'Şifre Giriniz',
-                  prefixIcon: Icon(Icons.lock),
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(width: 1, color: Colors.black),
-                      borderRadius:
-                          const BorderRadius.all(const Radius.circular(10.0))),
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(200, 50),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => musteriPage()),
-                  );
-                },
-                child: Text('GİRİŞ YAP'),
-              ),
-              SizedBox(height: 10),
-              TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => sifremiUnuttumPage()),
-                    );
-                  },
-                  child: Text(
-                    'Şifremi Unuttum',
-                    style: TextStyle(color: Colors.red),
-                  )),
-              TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => MKayitOlSayfasi()),
-                    );
-                  },
-                  child:
-                      Text('Kayıt Ol ', style: TextStyle(color: Colors.black)))
-            ]),
+      child: Scrollbar(
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text("MÜŞTERİ GİRİŞİ"),
+          ),
+          body: Center(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    logom,
+                    SizedBox(height: 20),
+                    TextField(
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 25),
+                      decoration: InputDecoration(
+                        contentPadding:
+                            const EdgeInsets.symmetric(vertical: 10.0),
+                        hintText: ' 05** *** ** **',
+                        prefixIcon: Icon(Icons.phone),
+                        border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.blueAccent),
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(10.0))),
+                      ),
+                      keyboardType: TextInputType.phone,
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      obscureText: true,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 25),
+                      decoration: InputDecoration(
+                        contentPadding:
+                            const EdgeInsets.symmetric(vertical: 10.0),
+                        hintText: 'Şifre Giriniz',
+                        prefixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.black),
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(10.0))),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(200, 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => musteriPage()),
+                        );
+                      },
+                      child: Text('GİRİŞ YAP'),
+                    ),
+                    SizedBox(height: 10),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => sifremiUnuttumPage()),
+                          );
+                        },
+                        child: Text(
+                          'Şifremi Unuttum',
+                          style: TextStyle(color: Colors.red),
+                        )),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MKayitOlSayfasi()),
+                          );
+                        },
+                        child: Text('Kayıt Ol ',
+                            style: TextStyle(color: Colors.black)))
+                  ]),
+            ),
           ),
         ),
       ),
